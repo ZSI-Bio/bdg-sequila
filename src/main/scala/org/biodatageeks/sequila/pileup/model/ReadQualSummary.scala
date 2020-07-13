36503a2df4ce22bd63a5826454bafdb2c00ed27b
agaszmurlo <amszmurlo@gmail.com>
2020-07-13 20:36:54 +0200

    handling deletions in base qual retreival

---
+++ src/main/scala/org/biodatageeks/sequila/pileup/model/ReadQualSummary.scala
@@ -0,0 +1,65 @@
+package org.biodatageeks.sequila.pileup.model
+
+import htsjdk.samtools.{Cigar, CigarOperator}
+
+case class ReadQualSummary (name:String, start: Int, end: Int, qualString: String, cigar: Cigar) {
+
+  def getBaseQualityForPosition(position: Int): Short = qualString.charAt(relativePosition(position)).toShort
+
+  def overlapsPosition(pos:Long):Boolean = isPositionInRange(pos) && !hasDeletionOnPosition(pos)
+
+  private def relativePosition(absPosition: Int):Int = absPosition - start - numDeletionsBeforePositions(absPosition)
+
+  private def isPositionInRange(pos:Long) = start<= pos && end >= pos
+
+  private def numDeletionsBeforePositions(pos: Int):Int = {
+    if(!cigar.containsOperator(CigarOperator.DELETION))
+      return 0
+
+    traverseCigar(cigar, pos, countDelsBeforePosition = true, hasDelOnPosition = false)
+  }
+
+  def hasDeletionOnPosition(pos:Long):Boolean = {
+    if(!cigar.containsOperator(CigarOperator.DELETION))
+      return false
+
+    if (traverseCigar(cigar,pos,countDelsBeforePosition = false, hasDelOnPosition = true) == 0)  false else true
+  }
+
+  private def traverseCigar(cigar:Cigar, pos: Long, countDelsBeforePosition: Boolean, hasDelOnPosition: Boolean): Int = {
+    val cigarIterator = cigar.iterator()
+    var positionFromCigar = start
+    var numDeletions = 0
+
+    while (cigarIterator.hasNext) {
+      val cigarElement = cigarIterator.next()
+      val cigarOperatorLen = cigarElement.getLength
+      val cigarOperator = cigarElement.getOperator
+
+      if (cigarOperator == CigarOperator.DELETION) {
+        val delStart = positionFromCigar
+        val delEnd = positionFromCigar + cigarOperatorLen
+
+        if (pos >= delStart && pos < delEnd && countDelsBeforePosition) {
+          val diff = pos.toInt - delStart
+          return numDeletions + diff
+        }
+        if (pos >= delStart && pos < delEnd && hasDelOnPosition)
+          return 1
+
+        numDeletions += cigarOperatorLen
+      }
+
+      positionFromCigar += cigarOperatorLen
+
+      if (positionFromCigar > pos && hasDelOnPosition)
+        return 0
+
+      if (positionFromCigar > pos && countDelsBeforePosition)
+        return numDeletions
+    }
+    0
+  }
+}
+
+
